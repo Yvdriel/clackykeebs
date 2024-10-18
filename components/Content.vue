bfd42dfca24a297347b8c82995059e97887263f7
yvdriel <yoran.22001@outlook.com>
2024-10-18 14:00:07 +0200

    Improve base for vendors

--- components/Content.vue
+++ components/Content.vue
@@ -1,35 +1,73 @@
 <template>
   <section class="bg-[#fef2f1] py-8 antialiased dark:bg-gray-900 md:py-12" id="vendors">
     <h1 class="text-center font-harman text-6xl md:text-8xl text-primary-text pb-4 md:pb-8">Vendors</h1>
-    <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+
+    <div class="
+      w-[90%]
+      block 
+      md:max-w-lg
+      m-auto 
+      p-4
+      px-8
+      bg-white 
+      border 
+      border-gray-200 
+      rounded-lg
+      overflow-hidden
+      text-center
+    ">
+      <div class="flex justify-between px-2 font-harman">
+        <div>region</div>
+        <div>Vendor</div>
+      </div>
+      <!-- <hr class="pb-4" /> -->
+      <div v-for="vendor in vendors" :to="vendor.link">
+
+        <hr class="my-1 pt-2" />
+        <div
+          class="w-full flex justify-between items-center p-2 hover:rounded-lg hover:cursor-pointer hover:bg-gray-100 ease-in-out duration-75 hover:scale-105">
+          <div class="flex items-center gap-x-4">
+            <div v-html="vendor.country.icon"></div>
+            <div class="">
+              {{ vendor.country.name }}
+            </div>
+          </div>
+          <div class="flex items-center gap-x-2">
+            <h1 class="font-bold">{{ vendor.name }}</h1>
+            <div class="opacity-50 hidden sm:block">|</div>
+            <img class="w-10 h-10 hidden sm:block" :src="vendor.logo" alt="" />
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div v-if="false" class="mx-auto max-w-screen-xl px-4 2xl:px-0">
       <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-        <nuxt-link
-        v-if="false"
-          v-for="vendor in vendors"
-          :to="vendor.link"
+        <nuxt-link v-if="false" v-for="vendor in vendors" :to="vendor.link"
           class="rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 hover:scale-105 ease-in-out duration-100">
           <div class="p-6">
             <div class=" md:h-56 w-full">
-                <img class="mx-auto h-full" :src="vendor.logo"
-                  alt="" />
+              <img class="mx-auto h-full" :src="vendor.logo" alt="" />
             </div>
           </div>
-          <div :style="vendor.country.flag" class="px-6 py-10 flex items-center justify-center bg-primary-text border-gray-200 border-b border-x rounded-b-lg bg-center bg-no-repeat bg-cover">
-            <h3 class="text-2xl font-extrabold leading-tight text-white text-center">{{vendor.country.name}}</h3>
+          <div :style="vendor.country.flag"
+            class="px-6 py-10 flex items-center justify-center bg-primary-text border-gray-200 border-b border-x rounded-b-lg bg-center bg-no-repeat bg-cover">
+            <h3 class="text-2xl font-extrabold leading-tight text-white text-center">{{ vendor.country.name }}</h3>
           </div>
         </nuxt-link>
-        <nuxt-link
-          v-for="vendor in vendors"
-          :to="vendor.link"
+        <nuxt-link v-for="vendor in vendors" :to="vendor.link"
           class="rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 hover:scale-105 ease-in-out duration-100">
           <div class="">
             <div class="rounded w-full">
-                <img class="mx-auto h-full rounded-t-lg" src="https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png"
-                  alt="" />
+              <img class="mx-auto h-full rounded-t-lg"
+                src="https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png" alt="" />
             </div>
           </div>
-          <div class="px-6 py-4 flex items-center justify-center border-gray-200 border-b border-x rounded-b-lg bg-center bg-no-repeat bg-cover">
-            <h3 class="text-2xl font-extrabold leading-tight text-primary-text text-center">{{vendor.name}}</h3>
+          <div
+            class="px-6 py-4 flex items-center justify-center border-gray-200 border-b border-x rounded-b-lg bg-center bg-no-repeat bg-cover">
+            <h3 class="text-2xl font-extrabold leading-tight text-primary-text text-center">{{ vendor.name }}</h3>
           </div>
         </nuxt-link>
       </div>
@@ -38,80 +76,34 @@
 </template>
 
 <script lang="ts" setup>
-import pt from '~/assets/img/vendors/prototypistlogo.svg';
+import pt from '~/assets/img/vendors/prototypistsmall.svg';
 import keygem from '~/assets/img/vendors/keygemlogo.png';
 
 const vendors = ref([
   {
     name: 'proto[Typist]',
+    link: 'https://prototypist.net',
     logo: pt,
-    link: 'google.com',
     country: {
       name: 'UK',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png')"
+      icon: '<span class="fi fi-gb text-4xl rounded"></span>',
     }
   },
   {
-    name: 'keygem',
+    name: 'Keygem',
+    link: 'https://keygem.com',
     logo: keygem,
-    link: 'google.com',
     country: {
       name: 'EU',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/europe-flag-jpg-xl.jpg')"
+      icon: '<span class="fi fi-de text-4xl rounded"></span>',
     }
   },
   {
-    name: 'proto[Typist]',
-    logo: pt,
-    link: 'google.com',
+    name: 'NovelKeys',
+    link: 'https://novelkeys.com',
     country: {
-      name: 'UK',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png')"
-    }
-  },
-  {
-    name: 'keygem',
-    logo: keygem,
-    link: 'google.com',
-    country: {
-      name: 'EU',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/europe-flag-jpg-xl.jpg')"
-    }
-  },
-  {
-    name: 'proto[Typist]',
-    logo: pt,
-    link: 'google.com',
-    country: {
-      name: 'UK',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png')"
-    }
-  },
-  {
-    name: 'keygem',
-    logo: keygem,
-    link: 'google.com',
-    country: {
-      name: 'EU',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/europe-flag-jpg-xl.jpg')"
-    }
-  },
-  {
-    name: 'proto[Typist]',
-    logo: pt,
-    link: 'google.com',
-    country: {
-      name: 'UK',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/united-kingdom-flag-png-large.png')"
-    }
-  },
-  {
-    name: 'keygem',
-    logo: keygem,
-    link: 'google.com',
-    country: {
-      name: 'EU',
-      flag: "background-image: url('https://www.countryflags.com/wp-content/uploads/europe-flag-jpg-xl.jpg')"
+      name: 'US',
+      icon: '<span class="fi fi-us text-4xl rounded"></span>',
     }
   },
 ]);
@@ -119,5 +111,4 @@ const vendors = ref([
 
 <style>
 @import "/node_modules/flag-icons/css/flag-icons.min.css";
-
 </style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -5,31 +5,6 @@
       
         <img src="~/assets/img/horizontallogo.svg" class="h-28 sm:h-36" alt="ClackyKeebs Logo" />
       </nuxt-link>
-      <p class="my-6 text-gray-500 dark:text-gray-400">Een uitgebreide reeks premium diensten en technieken, ontwikkeld
-        door DECustoms, om uw auto grondig te reinigen en te beschermen voor een schitterend en langdurig resultaat.</p>
-      <ul class="hidden flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white" v-if="false">
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Premium</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">Campaigns</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Blog</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Affiliate Program</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">FAQs</a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">Contact</a>
-        </li>
-      </ul>
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#"
           class="hover:underline">ClackyKeebs B.V.</a> All Rights Reserved.</span>
     </div>
